8f216f2eaf5434bc4c32f7ff6bc83e6ab7b61ba1
nTekin-coder <kartal.tekin@hotmail.com>
2025-02-19 22:37:41 +0100

    Fix Flask Port for Render

--- app.py
+++ app.py
@@ -67,5 +67,8 @@ def download():
     else:
         return "Keine Ergebnisse vorhanden."
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
